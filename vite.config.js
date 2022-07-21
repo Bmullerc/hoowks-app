47c0b48f9665dcb91f14118d576d08b5ee2c24e1
Bernardo Muller <bernardo.muller.bm@gmail.com>
2022-07-21 16:03:14 -0300

    Adding Vitest to play around with testing

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: '../dist'
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['../.test/setup.js'],
+    include: ['**/*(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
+  },
 })
