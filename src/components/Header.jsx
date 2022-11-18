2b07038a888f3bdc53b3edb2c6cef07ffcec5a85
Bernardo Muller <bernardo.muller.bm@gmail.com>
2022-11-18 10:17:39 -0300

    Learning Framer Motion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,39 @@
 import Logo from "../img/Hoowks.png"
+import { motion } from "framer-motion"
 
 export function Header() {
   return (
     <header id="header" className="flex justify-center items-center h-screen dark:bg-zinc-700 text-zinc-700 bg-zinc-200 tracking-tight dark:bg-gradient-to-b from-current to-orange-400 bg-gradient-to-t">
-      <div className="text-center text-zinc-800">
-        <img
+      <motion.div
+        initial={{ x: 0, y: 0, scale: .7 }}
+        whileInView={{ scale: 1.15, transition: { delay: .3, duration: .5 } }}
+        className="text-center text-zinc-800">
+        <motion.img
+          initial={{ scale: .7 }}
+          whileInView={{ scale: 1.2, rotate: 15, x: 10, transition: { delay: .2, duration: .7 } }}
+          whileHover={{rotate: 210}}
+          whileTap={{scale: .7}}
           src={Logo}
           alt="Hoowks Logo, a mix of a hook and an question mark"
-          className="inline-block lg:h-40 h-24 hover:scale-125 hover:-translate-y-6 duration-300"
+          className="inline-block lg:h-40 h-24"
         />
         <h1 className="lg:text-7xl text-6xl font-bold">
           React<br></br>
-          <span className="text-orange-400 py-1 tracking-wider uppercase lg:text-8xl text-7xl inline-block md:hover:scale-125 hover:translate-y-5 lg:hover:-translate-x-6 lg:hover:translate-y-1 transition-all duration-200 drop-shadow-lg">
+          <span className="text-orange-400 py-1 tracking-wider uppercase lg:text-8xl text-7xl inline-block drop-shadow-lg">
             Hooks
           </span> <span className=" lg:text-4xl text-3xl">and</span>
-          <span className="text-orange-300 pl-4 tracking-tighter uppercase lg:text-6xl text-6xl inline-block md:hover:scale-125 transition-all hover:translate-x-4 duration-200 drop-shadow-md">
+          <span className="text-orange-300 pl-4 tracking-tighter uppercase lg:text-6xl text-6xl inline-block drop-shadow-md">
             How
           </span>
         </h1>
         <h2 className="lg:text-5xl text-4xl font-bold">to
-          <span className="inline-block px-4 py-1 text-orange-200 tracking-wider font-bold uppercase lg:text-6xl text-5xl transition-all duration-200 md:hover:scale-125 hover:translate-y-2 ease-in-out drop-shadow-md">
+          <span className="inline-block px-4 py-1 text-orange-200 tracking-wider font-bold uppercase lg:text-6xl text-5xl drop-shadow-md">
             Use
           </span>
           them.
         </h2>
         <h4 className="mt-2 font-normal text-orange-100 drop-shadow-sm tracking-wide">A more practical approach</h4>
-      </div>
-    </header>
+      </motion.div>
+    </header >
   )
 }
